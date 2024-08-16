4bec28c0511de3e98b23895c5597e6e6c5e56a16
Gishisknown <gishisknown@gmail.com>
2024-08-15 20:03:52 -0400

    rizz commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,16 @@
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("fanum");
+        Scanner sc = new Scanner(System.in);
+        System.out.println("welcome to the rizz store:");
+        System.out.println("rizz: $40");
+        System.out.println("what would you like to buy");
+        String order = sc.nextLine();
+        if (order.toLowerCase() == "rizz") {
+            System.out.println("your total is $40");
+            
+        } else {
+            System.out.println("that aint on the menu fn");
+        }
     }
 }
